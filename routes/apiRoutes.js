a31d4afd9097ad6e2cf015485f945daef62163a0
Liz Emerson <uknity@gmail.com>
2021-04-15 19:47:26 -0400

    finalized api routes page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,38 @@
-const  = require('../data/tableData');
-const  = require('../data/waitinglistData');
+//const notes = require('../db/db.json');
+//require fs 
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+
+module.exports = (app) => {
+
+    app.get('/api/notes', (req, res) => {
+        //fs readfile db with its own file with err / data
+        fs.readFile("db/db.json","utf8", (err, data) => res.json(JSON.parse(data)));
+       
+    });
+
+    app.post('/api/notes', (req, res) => {
+        fs.readFile("db/db.json", "utf8", (err, data) =>{
+//convert data to Array
+            const noteArr = JSON.parse(data);
+
+            // read new Note from req.body
+            const newNote = req.body;
+
+
+            // add a unique id to the new Note
+            newNote.id = uuidv4();
+
+    // push the new note to the notes Array
+            noteArr.push(newNote);
+    // write the notes Array to the db.json file (remember to use JSON.stringify)
+            fs.writeFileSync("db/db.json", JSON.stringify(noteArr), "utf8");
+        });
+
+        
+    })
+    // post route
+    // read all data from db.json file (remember to use JSON.parse)
+   
+}
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = 8080;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
- console.log("Server listening on port 5000");
+ console.log("Server listening on port 8080");
 });
